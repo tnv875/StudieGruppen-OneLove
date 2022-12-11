b4d8d3590c854b453d69053beb0824621cdffbd1
riegleren <83757379+riegleren@users.noreply.github.com>
2022-12-11 09:43:21 +0100

    reordered code, added extra prints

--- a2-handout/test_util.c
+++ a2-handout/test_util.c
@@ -10,7 +10,9 @@ int main() {
   FILE *f_points = fopen("10_2.points", "r"); 
   assert(f_points != NULL);
 
-  int n, d, k;
+  int n, d;
+
+  int k = 2;
 
   const double* points = read_points(f_points, &n, &d);
   assert(points != NULL);
@@ -24,12 +26,18 @@ int main() {
   // use 7th point as our query
   const double* query = &(points[7*d]);
       
-  insert_if_closer(k, d, points, closest, query, 3);
-  insert_if_closer(k, d, points, closest, query, 2);
   insert_if_closer(k, d, points, closest, query, 1);
+  insert_if_closer(k, d, points, closest, query, 2);
+  insert_if_closer(k, d, points, closest, query, 3);
 
   // Print query
-  printf("Our query location: %f %f\n\n", query[0], query[1]);
+  printf("\nOur query:\n");
+  printf("Point 7: %f %f\n\n", query[0], query[1]);
+
+  printf("Comparison points:\n");
+  printf("Point 1: 0.202671 0.586538\n"); 
+  printf("Point 2: 0.833359 0.954286\n"); 
+  printf("Point 3: 0.146108 0.334634\n"); 
 
   // Print indexes in closest
   printf("Indexes in 'closest': \n");
@@ -44,11 +52,10 @@ int main() {
   for (int i = 0; i < k; i++) {  
     int ind = d * closest[i]; // multiply by d to fix dimensions
     const double* point = &(points[ind]);
-    printf("Point %d in closest: %f %f\n", i, point[0], point[1]);
+    printf("%d. in closest: %f %f\n", i, point[0], point[1]);
   } 
 
   // Note to self: insert_if_closer correctly inserts points in empty
   // spaces, but from then on does not correctly evaluate whether to 
   // update based on distances
 }
- 
