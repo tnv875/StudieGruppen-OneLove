b1f412acebe2e30af433676e27c1318efc51ddaf
riegleren <83757379+riegleren@users.noreply.github.com>
2022-12-13 18:10:17 +0100

    util.c now maintains sorted closest[]

--- a2-handout/util.c
+++ a2-handout/util.c
@@ -1,10 +1,9 @@
 #include "util.h"
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
-// Attention: Everytime we index into points[], we must multiply with d
-// For example, to get to point 3, we do &(points[3 * d])
 
 // √( ∑ (x[i]-y[i])² )
 double distance(int d, const double *x, const double *y) {
@@ -15,42 +14,60 @@ double distance(int d, const double *x, const double *y) {
   return sqrt(s);
 }
 
+
+// Auxillary function to sort first val in Closest 
+// to correct position (requires Closest is empty or already sorted)
+int sort_first(int *closest, int k, int d, const double *points,
+                    const double *query) {
+  double dist_candidate = distance(d, query, &(points[closest[0] * d]));
+  double dist_comp;
+  int tmp;
+  for (int i=1; i<k; i++) {
+
+    if (closest[i] == -1) {
+      // swap
+      tmp = closest[i-1];
+      closest[i-1] = closest[i];
+      closest[i] = tmp;
+      continue;
+    }
+    // break if we are done
+    dist_comp = distance(d, query, &(points[closest[i] * d]));
+    if (dist_comp < dist_candidate) {
+      return 0; // return now if we are done sorting
+    }
+
+    // swap if no other conditions are met
+    tmp = closest[i-1];
+    closest[i-1] = closest[i];
+    closest[i] = tmp;
+  }
+  return 1; //if candidate is moved to the very end
+}
+
+
 int insert_if_closer(int k, int d,
                      const double *points, int *closest, 
                      const double *query, int candidate) {
 
-  // distance to candidate from query
-  double dist_candidate = distance(d, query, &(points[candidate * d])); 
-  
-  // furthest distance in closest seen so far
-  double furthest = -1.0;
-  int furthest_i = 0;
-
-  // loop through all indexes in closest
-  for (int i = 0; i < k; i++) {
-    // if closest has empty spaces, trivially insert candidate and return 1
-    if (closest[i] == -1) {
-      closest[i] = candidate;
-      return 1;
-    } 
-    // calculate distance between query and the i'th point 
-    int i_index = closest[i];
-    const double* i_point = &(points[i_index * d]);
-    double dist_i = distance(d, query, i_point);
-
-    // check if we need to update furthest
-    if (furthest < dist_i) {
-      furthest = dist_i;
-      furthest_i = i;
-    }   
-  }
-
-  // inserts candidate in 'closest' if the distance to the candidate is smaller
-  // than furthest
-  if (furthest > dist_candidate) {
-    closest[furthest_i] = candidate;
+  // if first spot is empty (closest has empty spaces)
+  if (closest[0] == -1) {
+    closest[0] = candidate;
+    sort_first(closest, k, d, points, query);
     return 1;
-  } else {
-    return 0;
   }
+
+  // since Closest is already sorted in decending order, 
+  // we only need to compare with first elm as that is furthest
+
+  // distance to candidate from query
+  double dist_candidate = distance(d, query, &(points[ candidate * d])); 
+  double dist_furthest  = distance(d, query, &(points[closest[0] * d]));
+  if (dist_candidate < dist_furthest) {
+    closest[0] = candidate;
+    sort_first(closest, k, d, points, query);
+    return 1;
+  }
+  
+  return 0;
 }
