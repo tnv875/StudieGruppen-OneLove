47165c1cf4c29fe44bdf34587eca410503ec5bb7
riegleren <83757379+riegleren@users.noreply.github.com>
2022-12-22 11:30:23 +0100

    coord naive

--- a3/a3-handout/coord_query_naive.c
+++ a3/a3-handout/coord_query_naive.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <assert.h>
+#include <math.h>
 
 #include "record.h"
 #include "coord_query.h"
@@ -15,18 +16,40 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  assert(0);
-  // TODO
+  struct naive_data* data = malloc(sizeof(struct naive_data));
+  data->rs = rs;
+  data->n = n;
+  return data;
 }
+
 
 void free_naive(struct naive_data* data) {
-  assert(0);
-  // TODO
+  free(data->rs);
+  free(data);
+
 }
 
+double distance(double x_lon, double x_lat, double y_lon, double y_lat) { 
+  return sqrt(pow((x_lon - y_lon), 2) + pow((x_lat - y_lat), 2));
+}
+
+
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  assert(0);
-  // TODO
+  double* coord = malloc(2*sizeof(double));
+  coord[0] = lon;
+  coord[1] = lat;
+
+  double closest_dist = INFINITY; // index of closest record encountered so far
+  int closest_i = -1; // index of closest record encountered so far
+
+  for (int i = 0; i < data->n; i++) {
+    double dist = distance(lon, lat, data->rs[i].lon, data->rs[i].lat);
+    if (dist < closest_dist) {
+      closest_dist = dist;
+      closest_i = i;
+    }
+  }
+  return &data->rs[closest_i];
 }
 
 int main(int argc, char** argv) {
