835b57c3b28aaa4a1a286576f6d18d42210a0b48
JJGeHa <tnv875@alumni.ku.dk>
2023-01-07 10:07:03 +0100

    Delete tests

--- a4/test2.py
+++
@@ -1,5 +0,0 @@
-footballers_goals = {'Eusebio': 120, 'Cruyff': 104, 'Pele': 150, 'Ronaldo': 132, 'Messi': 125}
-
-sorted_footballers_by_goals = sorted(footballers_goals.items(), key=lambda x:x[1], reverse=True)
-
-print(sorted_footballers_by_goals)
